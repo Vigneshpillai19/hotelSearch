608bf6197e147a46e858e7999331016a8e39a562
Vigneshpillai19 <43498822+Vigneshpillai19@users.noreply.github.com>
2020-06-30 12:41:31 +0530

    Update yelp.js

--- src/api/yelp.js
+++ src/api/yelp.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 export default axios.create({
     baseURL: 'https://api.yelp.com/v3/businesses',
     headers: {
-        Authorization: 'Bearer SpsCrKLMBGHfZz7jlhNWQ6dQMsyb9DMQvDYrKHyvWloWVu72eBH9aZEVnC2tiRe6dZKiuDlrT4CPY5kXBYMA9WaoPSPeJXHGE8zOM5o2RHU98Lh1XwQDIRTJabF_XnYx'
+        Authorization: 'Bearer YOUR_SECRET_KEY'
     }
     // baseURL: 'https://jsonplaceholder.typicode.com',
 });
